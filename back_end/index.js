200773590911de275aad1dca4e77438144ee0f7d
MoayadMaher <its.moayadj@gmail.com>
2024-01-30 02:05:59 +0300

    CORS fixing

--- back_end/index.js
+++ back_end/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+    origin: 'http://localhost:5500',
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 app.get('/', async (req, res) => {
     const city = req.query.city;
--- front_end/index.js
+++ front_end/index.js
@@ -1,15 +1,27 @@
+function initState() {
+    document.getElementById('temperature').innerText = '';
+    document.getElementById('description').innerText = '';
+    document.getElementById('icon').src = './loupe.png';
+    document.getElementById('errorDisplay').innerText = '';
+}
+initState();
+
+
 document.getElementById('getWeather').addEventListener('click', async function() {
     let city = document.getElementById('city').value;
     try {
        const weatherResponse = await fetch('http://localhost:3000?city=' + city);
-         const weatherData = await weatherResponse.json();
+       const weatherData = await weatherResponse.json();
 
-         document.getElementById('temperature').innerText = weatherData.temperature;
+         const temperature = Math.round(weatherData.temperature);
+        document.getElementById('temperature').innerText = temperature + '°C';
          document.getElementById('description').innerText = weatherData.description;
          document.getElementById('icon').src = weatherData.icon;
 
     } catch (error) {
-        alert('City not found');
+        initState();
+        document.getElementById('errorDisplay').innerText = "City not found";
     }
 
 });
+
